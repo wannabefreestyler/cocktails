df75d49d2fe5e8e8537eb2899c096364ff791536
wannabefreestyler <160882126+wannabefreestyler@users.noreply.github.com>
2024-05-06 23:01:03 +0300

    Delete cocktails directory

--- cocktails/src/main/java/com/example/cocktails/exception/ImageNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cocktails.exception;
-
-public class ImageNotFoundException extends Exception {
-  public ImageNotFoundException(String message) {
-    super(message);
-  }
-}
--- cocktails/src/main/java/com/example/cocktails/exception/IngredientNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.cocktails.exception;
-
-public class IngredientNotFoundException extends Exception {
-  public IngredientNotFoundException(String message) {
-    super(message);
-  }
-}
